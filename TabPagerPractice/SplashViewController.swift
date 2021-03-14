3153085b225ff434d467c3932c810acc57923f21
松尾淳平 <matsuojumpei@matsuojunpeinoMacBook-Pro.local>
2021-03-14 22:54:57 +0900

    起動画面作成

--- TabPagerPractice/SplashViewController.swift
+++ TabPagerPractice/SplashViewController.swift
@@ -11,7 +11,7 @@ import RxSwift
 class SplashViewController: UIViewController {
 //    var imageView: UIImageView!
     var label:UILabel!
-
+    var aa:UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,7 +29,7 @@ class SplashViewController: UIViewController {
                         self.label.text = "EX"
         self.label.font = UIFont(name: "Arial-BoldMT", size: 150)
         self.label.textAlignment = .center
-        let aa = CustomView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        aa = CustomView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
         aa.center = self.view.center
         aa.backgroundColor = .white
         
@@ -41,7 +41,19 @@ class SplashViewController: UIViewController {
                         self.view.addSubview(aa)
         self.label.textColor = .orange
         self.view.backgroundColor = .white
+        
+        UIView.animate(withDuration: 1,
+                       delay: 1.1,
+                               options: UIView.AnimationOptions.curveEaseOut,
+                               animations: { () in
+                                self.aa.transform = CGAffineTransform(scaleX: 8.0, y: 8.0)
+                                self.aa.alpha = 0
+                }, completion: { (Bool) in
+                    //で、アニメーションが終わったらimageViewを消す
+                    self.aa.removeFromSuperview()
+                })
     }
+    weak var shapeLayer: CAShapeLayer?
     func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
            return CGRect(x: x, y: y, width: width, height: height)
        }
@@ -49,14 +61,47 @@ class SplashViewController: UIViewController {
     class CustomView:UIView{
         override func draw(_ rect: CGRect) {
             let path = UIBezierPath()
-            path.move(to: CGPoint(x: rect.midX + 14, y: rect.maxY - 99))
-            path.addLine(to: CGPoint(x: rect.midX + 83, y: rect.minY + 94))
+            path.move(to: CGPoint(x: 198.5, y: 150))
+            path.addLine(to: CGPoint(x: 302, y: -3))
             path.close()
-            path.lineWidth = 10
+            path.lineWidth = 24
             UIColor.red.setStroke()
-            path.stroke()
+            
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.fillColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+            shapeLayer.strokeColor = UIColor.orange.cgColor
+            shapeLayer.lineWidth = 24
+            shapeLayer.path = path.cgPath
+
+            self.layer.addSublayer(shapeLayer)
+            let animation = CABasicAnimation(keyPath: "strokeEnd")
+            animation.fromValue = 0
+            animation.duration = 1
+            shapeLayer.add(animation, forKey: "MyAnimation")
+            
+            let path2 = UIBezierPath()
+            path2.move(to: CGPoint(x: 198.5, y: 150))
+            path2.addLine(to: CGPoint(x: 95, y: 303))
+            path2.close()
+            path2.lineWidth = 24
+            UIColor.red.setStroke()
+            
+            let shapeLayer2 = CAShapeLayer()
+            shapeLayer2.fillColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+            shapeLayer2.strokeColor = UIColor.orange.cgColor
+            shapeLayer2.lineWidth = 24
+            shapeLayer2.path = path2.cgPath
+
+            // animate it
+
+            self.layer.addSublayer(shapeLayer2)
+            let animation2 = CABasicAnimation(keyPath: "strokeEnd")
+            animation2.fromValue = 0
+            animation2.duration = 1
+            shapeLayer2.add(animation2, forKey: "MyAnimation2")
+
         }
-    }
+        }    }
     
 
     /*
@@ -69,4 +114,4 @@ class SplashViewController: UIViewController {
     }
     */
 
-}
+
